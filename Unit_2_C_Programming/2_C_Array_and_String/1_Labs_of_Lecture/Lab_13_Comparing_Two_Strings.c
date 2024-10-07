5b1143a3905679078d33dfc42833154da7cb61c2
Mohamed Elassy <muhamedasi18@gmail.com>
2024-10-07 03:40:15 +0300

    The Program about Comparing Two Strings

---
+++ Unit_2_C_Programming/2_C_Array_and_String/1_Labs_of_Lecture/Lab_13_Comparing_Two_Strings.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Lab_13_Comparing_Two_Strings.c
+ Author      : Mohamed Elassy
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
